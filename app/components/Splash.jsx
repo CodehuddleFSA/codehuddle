9a0f64b5dbb5a507f9d4513cfa7e382c5f878946
Evan DiGiambattista <edigiam@me.com>
2017-01-25 15:46:31 -0500

    Add SASS & Materialize-css and create Splash.jsx

---
+++ app/components/Splash.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export const Splash = () => {
+	
+};
+
+import {connect} from 'react-redux';
+
+const mapStateToProps = () => {};
+const mapDispatchToProps = () => {};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Splash);
