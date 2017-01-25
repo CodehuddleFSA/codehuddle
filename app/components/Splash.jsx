69830b7786de5594fda6f664711d28d58981edb6
Evan DiGiambattista <edigiam@me.com>
2017-01-25 15:53:25 -0500

    add jquery

--- app/components/Splash.jsx
+++ app/components/Splash.jsx
@@ -1,7 +1,26 @@
 import React from 'react';
 
 export const Splash = () => {
-	
+	return (
+		<nav>
+    <div className="nav-wrapper">
+      <a href="#!" className="brand-logo">Logo</a>
+      <a href="#" data-activates="mobile-demo" className="button-collapse"><i className="material-icons">menu</i></a>
+      <ul className="right hide-on-med-and-down">
+        <li><a href="sass.html">Sass</a></li>
+        <li><a href="badges.html">Components</a></li>
+        <li><a href="collapsible.html">Javascript</a></li>
+        <li><a href="mobile.html">Mobile</a></li>
+      </ul>
+      <ul className="side-nav" id="mobile-demo">
+        <li><a href="sass.html">Sass</a></li>
+        <li><a href="badges.html">Components</a></li>
+        <li><a href="collapsible.html">Javascript</a></li>
+        <li><a href="mobile.html">Mobile</a></li>
+      </ul>
+    </div>
+  </nav>
+	);
 };
 
 import {connect} from 'react-redux';
