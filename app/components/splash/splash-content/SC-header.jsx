537990cf5f96d9565e6aa6e72db65aa2d791cc06
Evan DiGiambattista <edigiam@me.com>
2017-01-27 16:47:33 -0500

    add favicon and new title logo

--- app/components/splash/splash-content/SC-header.jsx
+++ app/components/splash/splash-content/SC-header.jsx
@@ -4,7 +4,10 @@ export default () => {
 	return (
 		<div id="splash-header" className="container center-align">
 			<div className="row">
-				<h1 className="animated fadeIn col s12 section"><i className="material-icons medium">desktop_mac</i> Code Huddle</h1>
+				<ul id="splash-h1-ul">
+					<li id="splash-h1-ul-svg"><img className="animated fadeIn" src="/images/code-outline.svg"/></li>
+					<li id="splash-h1-ul-h1"><h1 className="animated fadeIn col s12 section">Code Huddle</h1></li>
+				</ul>
 			</div>
 			<h5 className="animated fadeIn">Technical Interviews Done Right</h5>
 			<a href="#body-1" id="header-down-arrow-icon" className="animated fadeIn center-align"><i className="medium material-icons">keyboard_arrow_down</i></a>
