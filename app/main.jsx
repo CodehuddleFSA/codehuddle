0a523cdc13229c3f7acdfa6b540ee145f8d31518
Evan DiGiambattista <edigiam@me.com>
2017-01-30 12:19:11 -0500

    modify main.jsx to accomodate editor/canvas/redux merge

--- app/main.jsx
+++ app/main.jsx
@@ -1,37 +1,22 @@
 'use strict';
 import React from 'react';
-import {Router, Route, IndexRedirect, browserHistory} from 'react-router';
-import {render} from 'react-dom';
-import {connect, Provider} from 'react-redux';
+import { Router, Route, IndexRedirect, browserHistory } from 'react-router';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
 
 import store from './store';
-import Jokes from './components/Jokes';
 import Login from './components/Login';
 import WhoAmI from './components/WhoAmI';
 import Editor from './components/Editor';
 import Splash from './components/Splash/Splash';
 
-const ExampleApp = connect(
-  ({ auth }) => ({ user: auth })
-) (
-  ({ user, children }) =>
-    <div>
-      <nav>
-        {user ? <WhoAmI/> : <Login/>}
-      </nav>
-      {children}
-    </div>
-)
-
 render (
   <Provider store={store}>
     <Router history={browserHistory}>
       <Route path="/" component={Splash}>
-        {/*<IndexRedirect to="/jokes" /> */}
-        <Route path="/jokes" component={Jokes} />
         <Route path="/editor" component={Editor} />
       </Route>
     </Router>
   </Provider>,
   document.getElementById('main')
-)
+);
