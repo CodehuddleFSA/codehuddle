c8cef36c0230738070b464eeb55015705258267a
Evan DiGiambattista <edigiam@me.com>
2017-01-30 14:58:24 -0500

    finish basic render of section 1

--- app/components/splash/Splash.jsx
+++ app/components/splash/Splash.jsx
@@ -1,10 +1,8 @@
 'use strict';
 
 import React from 'react';
-// import SplashNav from './Splash-Nav';
 import SplashNav1 from './Splash-Nav-1';
 import SplashContent from './Splash-Content';
-// import SplashFooter from './Splash-Footer';
 
 export const Splash = (props) => {
 	
@@ -12,7 +10,7 @@ export const Splash = (props) => {
 		<div id="splash-root" className="page-flexbox-wrapper">
 			<SplashNav1/>
 			<SplashContent/>
-			{/*<SplashFooter/>*/}
+
 		</div>
 	);
 };
--- app/components/splash/splash-content/SC-content-1.jsx
+++ app/components/splash/splash-content/SC-content-1.jsx
@@ -4,13 +4,17 @@ import React from 'react';
 
 export default () => {
 	return (
-		<div id="splash-content-1">
-			<div className="center-align">
-				<div id="splash-content-1-header" className="row">
-					<h3 className="col s12 m6 l6">Code Editor</h3>
-					<h3 className="col s12 m6 l6">White Board</h3>
+		<div id="splash-content-1" className="valign-wrapper">
+				<div id="splash-content-1-header" className="row center-align valign">
+					<div className="col s12 m6 l6">
+						<h3>Ace Code Editor</h3>
+						<i className="material-icons large">code</i>
+					</div>
+					<div className="col s12 m6 l6">
+						<h3>Built-In White Board</h3>
+						<i className="material-icons large">palette</i>
+					</div>
 				</div>
-			</div>
 		</div>
 	);
 };
