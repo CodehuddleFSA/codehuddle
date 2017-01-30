2785163b6c191372e4778ee143c0a769dd56c95e
Evan DiGiambattista <edigiam@me.com>
2017-01-30 12:45:31 -0500

    update sass rules

--- app/components/splash/splash-content/SC-content-1.jsx
+++ app/components/splash/splash-content/SC-content-1.jsx
@@ -7,9 +7,8 @@ export default () => {
 		<div id="splash-content-1">
 			<div className="center-align">
 				<div id="splash-content-1-header" className="row">
-					<h2 className="col s12 m5 l5">Code Editor</h2>
-					<h2 className="col s12 m2 l2"> + </h2>
-					<h2 className="col s12 m5 l5">White Board</h2>
+					<h3 className="col s12 m6 l6">Code Editor</h3>
+					<h3 className="col s12 m6 l6">White Board</h3>
 				</div>
 			</div>
 		</div>
