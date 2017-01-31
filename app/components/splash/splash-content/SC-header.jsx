a9e86fc6f36b248071328d990b052a180f84e299
Evan DiGiambattista <edigiam@me.com>
2017-01-31 18:07:31 -0500

    fix button link

--- app/components/splash/splash-content/SC-header.jsx
+++ app/components/splash/splash-content/SC-header.jsx
@@ -15,7 +15,7 @@ export default () => {
 			<h4 className="animated fadeIn">Technical Interviews Done Right</h4>
 			<div className="row">
 				<ul id="get-started-btn">
-					<li><a className="animated fadeIn waves-effect waves-light btn-large" href="/interviewroom"><i className="material-icons right">navigate_next</i>Get Started</a></li>
+					<li><a className="animated fadeIn waves-effect waves-light btn-large" href="/interviewRoom"><i className="material-icons right">navigate_next</i>Get Started</a></li>
 				</ul>
 			</div>
 			<a href="#splash-content-1" id="header-down-arrow-icon" className="animated fadeIn center-align"><i className="medium material-icons">keyboard_arrow_down</i></a>
