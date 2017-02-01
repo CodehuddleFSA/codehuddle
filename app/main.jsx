57320e4c6228b47c615b22a9992599d39d116c35
Evan DiGiambattista <edigiam@me.com>
2017-02-01 16:04:48 -0500

    fix interview-room dir

--- app/main.jsx
+++ app/main.jsx
@@ -7,14 +7,8 @@ import { Provider } from 'react-redux';
 import store from './store';
 import Login from './components/Login';
 import WhoAmI from './components/WhoAmI';
-<<<<<<< HEAD
 import Splash from './components/splash/Splash';
-import InterviewRoom from './components/InterviewRoom';
-
-=======
-import Splash from './components/Splash/Splash';
 import InterviewRoom from './components/interview-room/InterviewRoom';
->>>>>>> b469c51db8d692b754e6d73d4fbc40fe838bfed1
 import { socketsJoinRoom } from 'APP/app/sockets';
 
 function interviewOnEnter (nextState) {
