e7269965117759d24f950db8c85102fe5aa57a7f
Evan DiGiambattista <edigiam@me.com>
2017-02-03 15:24:46 -0500

    Add working drawers with original options panel

--- app/components/InterviewRoomOptions.jsx
+++ app/components/InterviewRoomOptions.jsx
@@ -1,7 +1,22 @@
 // Required libraries
 import React from 'react';
+import Checkbox from 'material-ui/Checkbox';
+import ActionFavorite from 'material-ui/svg-icons/action/favorite';
+import ActionFavoriteBorder from 'material-ui/svg-icons/action/favorite-border';
+import Visibility from 'material-ui/svg-icons/action/visibility';
+import VisibilityOff from 'material-ui/svg-icons/action/visibility-off';
 
 /* -----------------    COMPONENT     ------------------ */
+
+const styles = {
+  block: {
+    padding: 20,
+    maxWidth: 250
+  },
+  checkbox: {
+    marginBottom: 16
+  }
+};
 
 export const InterviewRoomOptions = ({ options, setOptions }) => {
   return (
