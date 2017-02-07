c91201cadb6fa98da19781244c0af1c4f4880e54
Andrew Garcia <runandrew@gmail.com>
2017-02-07 14:58:37 -0500

    add cnadidate name to table

--- app/components/InterviewList.jsx
+++ app/components/InterviewList.jsx
@@ -35,7 +35,7 @@ export const InterviewList = ({ allInterviews }) => {
               <TableRow key={ interview.id }>
                 <TableRowColumn>{ interview.id }</TableRowColumn>
                 <TableRowColumn>{ new Date(interview.date).toLocaleDateString() }</TableRowColumn>
-                <TableRowColumn>Bob Interviewee</TableRowColumn> // TODO: change this to candidate name
+                <TableRowColumn>{ interview.candidateName }</TableRowColumn> // TODO: change this to candidate name
                   <TableRowColumn>{ interview.position }</TableRowColumn>
                   <TableRowColumn>{ interview.status }</TableRowColumn>
                   <TableRowColumn>
